3c89a289a938762c26777f0713665649abfb1467
leonidhr <hristenokleonid@gmail.com>
2023-03-17 20:21:49 +0200

    add catalog

--- js/app.js
+++ js/app.js
@@ -397,6 +397,9 @@
             }));
         }
         let bodyLockStatus = true;
+        let bodyLockToggle = (delay = 500) => {
+            if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
+        };
         let bodyUnlock = (delay = 500) => {
             let body = document.querySelector("body");
             if (bodyLockStatus) {
@@ -431,6 +434,14 @@
                 }), delay);
             }
         };
+        function menuInit() {
+            if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
+                if (bodyLockStatus && e.target.closest(".icon-menu")) {
+                    bodyLockToggle();
+                    document.documentElement.classList.toggle("menu-open");
+                }
+            }));
+        }
         function functions_FLS(message) {
             setTimeout((() => {
                 if (window.FLS) console.log(message);
@@ -697,15 +708,112 @@
                 }));
             }
         }), 0);
+        class DynamicAdapt {
+            constructor(type) {
+                this.type = type;
+            }
+            init() {
+                this.оbjects = [];
+                this.daClassname = "_dynamic_adapt_";
+                this.nodes = [ ...document.querySelectorAll("[data-da]") ];
+                this.nodes.forEach((node => {
+                    const data = node.dataset.da.trim();
+                    const dataArray = data.split(",");
+                    const оbject = {};
+                    оbject.element = node;
+                    оbject.parent = node.parentNode;
+                    оbject.destination = document.querySelector(`${dataArray[0].trim()}`);
+                    оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+                    оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+                    оbject.index = this.indexInParent(оbject.parent, оbject.element);
+                    this.оbjects.push(оbject);
+                }));
+                this.arraySort(this.оbjects);
+                this.mediaQueries = this.оbjects.map((({breakpoint}) => `(${this.type}-width: ${breakpoint}px),${breakpoint}`)).filter(((item, index, self) => self.indexOf(item) === index));
+                this.mediaQueries.forEach((media => {
+                    const mediaSplit = media.split(",");
+                    const matchMedia = window.matchMedia(mediaSplit[0]);
+                    const mediaBreakpoint = mediaSplit[1];
+                    const оbjectsFilter = this.оbjects.filter((({breakpoint}) => breakpoint === mediaBreakpoint));
+                    matchMedia.addEventListener("change", (() => {
+                        this.mediaHandler(matchMedia, оbjectsFilter);
+                    }));
+                    this.mediaHandler(matchMedia, оbjectsFilter);
+                }));
+            }
+            mediaHandler(matchMedia, оbjects) {
+                if (matchMedia.matches) оbjects.forEach((оbject => {
+                    this.moveTo(оbject.place, оbject.element, оbject.destination);
+                })); else оbjects.forEach((({parent, element, index}) => {
+                    if (element.classList.contains(this.daClassname)) this.moveBack(parent, element, index);
+                }));
+            }
+            moveTo(place, element, destination) {
+                element.classList.add(this.daClassname);
+                if ("last" === place || place >= destination.children.length) {
+                    destination.append(element);
+                    return;
+                }
+                if ("first" === place) {
+                    destination.prepend(element);
+                    return;
+                }
+                destination.children[place].before(element);
+            }
+            moveBack(parent, element, index) {
+                element.classList.remove(this.daClassname);
+                if (void 0 !== parent.children[index]) parent.children[index].before(element); else parent.append(element);
+            }
+            indexInParent(parent, element) {
+                return [ ...parent.children ].indexOf(element);
+            }
+            arraySort(arr) {
+                if ("min" === this.type) arr.sort(((a, b) => {
+                    if (a.breakpoint === b.breakpoint) {
+                        if (a.place === b.place) return 0;
+                        if ("first" === a.place || "last" === b.place) return -1;
+                        if ("last" === a.place || "first" === b.place) return 1;
+                        return 0;
+                    }
+                    return a.breakpoint - b.breakpoint;
+                })); else {
+                    arr.sort(((a, b) => {
+                        if (a.breakpoint === b.breakpoint) {
+                            if (a.place === b.place) return 0;
+                            if ("first" === a.place || "last" === b.place) return 1;
+                            if ("last" === a.place || "first" === b.place) return -1;
+                            return 0;
+                        }
+                        return b.breakpoint - a.breakpoint;
+                    }));
+                    return;
+                }
+            }
+        }
+        const da = new DynamicAdapt("max");
+        da.init();
         document.addEventListener("DOMContentLoaded", (() => {
+            showSearch();
+            addLimit("[data-limit]");
+        }));
+        function addLimit(elSelector) {
+            if (elSelector) document.querySelectorAll(elSelector).forEach((el => {
+                el.innerHTML = truncateText(el.innerHTML, 40);
+            }));
+        }
+        function truncateText(text, limit) {
+            if (text.length <= limit) return text; else return text.substring(0, limit) + "...";
+        }
+        function showSearch() {
             const searchForm = document.getElementById("search-form");
             const searchInput = document.getElementById("search-input");
-            searchInput.addEventListener("input", (e => {
+            if (searchForm) searchInput.addEventListener("input", (e => {
                 searchForm.classList.add("_input");
                 if ("" == searchInput.value) searchForm.classList.remove("_input");
             }));
-        }));
+        }
         window["FLS"] = true;
         isWebp();
+        menuInit();
     })();
 })();
