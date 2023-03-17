95ae20cb567a796cce6d31b17c669eff1d6d73fc
leonidhr <hristenokleonid@gmail.com>
2023-03-17 20:20:44 +0200

    add catalog

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,14 +5,37 @@ import { flsModules } from "./modules.js";
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  showSearch();
+  addLimit('[data-limit]');
+});
+
+function addLimit(elSelector) {
+  if (elSelector) {
+    document.querySelectorAll(elSelector).forEach(el => {
+      el.innerHTML = truncateText(el.innerHTML, 40);
+    });
+  }
+}
+
+function truncateText(text, limit) {
+  if (text.length <= limit) {
+    return text;
+  } else {
+    return text.substring(0, limit) + "...";
+  }
+}
+
+function showSearch() {
   const searchForm = document.getElementById('search-form');
   const searchInput = document.getElementById('search-input');
   
-  searchInput.addEventListener("input", e=> {
-    searchForm.classList.add('_input');
-
-    if (searchInput.value == '') {
-      searchForm.classList.remove('_input');
-    }
-  });
-});
+  if (searchForm) {
+    searchInput.addEventListener("input", e=> {
+      searchForm.classList.add('_input');
+  
+      if (searchInput.value == '') {
+        searchForm.classList.remove('_input');
+      }
+    });
+  }
+}
